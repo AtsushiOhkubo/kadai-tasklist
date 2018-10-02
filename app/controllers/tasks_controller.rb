6e63cba14816b83f808a85c6ec032cfa58fe6161
atsushi.ookubo <a2c009bo@gmail.com>
2018-10-02 15:56:07 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,26 +1,20 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show,:destroy,:edit,:update]
   
-  def index
-    @tasks=Task.all.page(params[:page])
-  end
 
   def show
   end
 
-  def new
-    @task = Task.new
-  end
-
   def create
-    @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
+      flash[:success] = 'メッセージを投稿しました。'
+      redirect_to root_url
     else
-      flash.now[:danger] = 'Task が投稿されませんでした'
-      render :new
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+      render 'toppages/index'
     end
   end
 
@@ -41,18 +35,19 @@ class TasksController < ApplicationController
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
+
+  def task_params
+    params.require(:task).permit(:content,:status)
   end
 
-  # Strong Parameter
-  def task_params
-    params.require(:task).permit(:content , :status)
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
-  
 end
